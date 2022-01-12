57e554b0f2ffea12714f7feada8750890ec69825
“hui1.huang” <hui1.huang@ximalaya.com>
2022-01-12 11:26:04 +0800

    打包react代码

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 // import '@babel/polyfill' // 兼容低版本的实现 像map方法
-const arr = [
-  new Promise(() => {}),
-  new Promise(() => {})
-]
+// const arr = [
+//   new Promise(() => {}),
+//   new Promise(() => {})
+// ]
 
-arr.map(item => {
-  console.log(item)
-})
+// arr.map(item => {
+//   console.log(item)
+// })
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  return <div>Hello World!</div>  
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
