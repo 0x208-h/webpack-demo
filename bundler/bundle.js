d3904882c5a1dee569c6067c860d30ebb80a7775
“hui1.huang” <hui1.huang@ximalaya.com>
2022-01-20 15:24:53 +0800

    完成

--- bundler/bundle.js
+++ bundler/bundle.js
@@ -43,23 +43,56 @@ const makeDependenciesGraph = (entry) => {
   for (let i = 0; i < graphArr.length; i++) {
     const item = graphArr[i];
     const { dependencies } = item;
-    if(dependencies) {
-      for(let j in dependencies) {
+    if (dependencies) {
+      for (let j in dependencies) {
         const module = moduleAnalyser(dependencies[j]);
-        graphArr.push(module)
+        graphArr.push(module);
       }
     }
   }
-  const graph = {}
-  graphArr.forEach(item => {
+  const graph = {};
+  graphArr.forEach((item) => {
     graph[item.filename] = {
       dependencies: item.dependencies,
-      code: item.code
-    }
-  })
+      code: item.code,
+    };
+  });
   // console.log(graphArr, graph, 'graph')
-  return graph
+  return graph;
 };
 
-const graphInfo = makeDependenciesGraph("./src/index.js");
-console.log(graphInfo)
+const generateCode = (entry) => {
+  const graph = JSON.stringify(makeDependenciesGraph(entry));
+  // return `
+  // (function(graph){
+  //   function require(module) {
+  //     function localRequire(relativePath) {
+  //       return require(graph[module].dependencies[relativePath])
+  //     }
+  //     var exports = {};
+  //     (function(require, exports, code){
+  //       eval(code)
+  //     })(localRequire, exports, graph[module].code)
+  //     return exports;
+  //   };
+  //   require('${entry})
+  // })(${graph})`;
+  return `
+		(function(graph){
+			function require(module) { 
+				function localRequire(relativePath) {
+					return require(graph[module].dependencies[relativePath]);
+				}
+				var exports = {};
+				(function(require, exports, code){
+					eval(code)
+				})(localRequire, exports, graph[module].code);
+				return exports;
+			};
+			require('${entry}')
+		})(${graph});
+	`;
+};
+
+const code = generateCode("./src/index.js");
+console.log(code);
